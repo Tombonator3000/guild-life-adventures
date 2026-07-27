c576900da19a173f5fc8a784fbb0343723d9add4
Tombonator3000 <126078233+Tombonator3000@users.noreply.github.com>
2026-07-27 09:34:55 +0200

    fix: preserve contextual gameplay tips

--- src/components/game/GameBoardAuxiliaryLayer.tsx
+++ src/components/game/GameBoardAuxiliaryLayer.tsx
@@ -6,6 +6,7 @@ import { DeathModal } from './DeathModal';
 import { PlayerInfoModal } from './PlayerInfoModal';
 import { UpdateBanner } from './UpdateBanner';
 import { ChatPanel } from './ChatPanel';
+import { ContextualTips } from './ContextualTips';
 import { SpectatorOverlay } from './SpectatorOverlay';
 import { TopDropdownMenu } from './TopDropdownMenu';
 import { TutorialOverlay } from './TutorialOverlay';
@@ -35,6 +36,7 @@ export function GameBoardAuxiliaryLayer({
   deathModalProps,
   playerInfoProps,
   chatProps,
+  showContextualTips,
   spectatorOverlayProps,
   topDropdownProps,
 }: GameBoardAuxiliaryLayerProps) {
@@ -60,6 +62,7 @@ export function GameBoardAuxiliaryLayer({
       {playerInfoProps && <PlayerInfoModal {...playerInfoProps} />}
       <UpdateBanner />
       {chatProps && <ChatPanel {...chatProps} />}
+      {showContextualTips && <ContextualTips />}
       {spectatorOverlayProps && <SpectatorOverlay {...spectatorOverlayProps} />}
       {topDropdownProps && <TopDropdownMenu {...topDropdownProps} />}
     </>
